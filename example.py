592a1d6000c52862445b5a48a6e5074dfb768334
mushnikov <mivspb55@gmail.com>
2023-02-09 21:09:03 +0300

    remove old uploading

--- example.py
+++ example.py
@@ -5,12 +5,12 @@ PORT = 9105         # Port for K-Roset
 
 robot = khirolib(IP, PORT, connection_mode='single', log=True)
 
-result = robot.upload_program_experimental(filename="as_programs/endless.as", kill_current_program=True)
-result = robot.upload_program_experimental(filename="as_programs/large.as", kill_current_program=True)
+result = robot.upload_program(filename="as_programs/endless.as", kill_current_program=True)
+result = robot.upload_program(filename="as_programs/large.as", kill_current_program=True)
 
 program_text = "var1 = 10\n" \
                "POINT loc1 = TRANS(100, 100, 100, 100)"
-robot.upload_program_experimental(program_name="prog", program_text=program_text, kill_current_program=False)
+robot.upload_program(program_name="prog", program_text=program_text, kill_current_program=False)
 
 robot.execute_pc_program(program_name="endless", thread=2)
 
