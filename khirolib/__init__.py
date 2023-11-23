dda6c30e65934252b94618473e50ae5bf6445067
aorehov <alexey.orehoff@gmail.com>
2023-11-23 16:09:47 +0300

    Moved files into new src directory

--- khirolib/__init__.py
+++ khirolib/__init__.py
@@ -1,34 +1,8 @@
-from irobot import *
+from src.irobot import *
 import threading
-import signal
-import sys
-from contextlib import contextmanager
-from typing import Callable, Any
-from khilib_internal import KHITelnetLib
+from utils.time_limit import time_limit
+from src.khilib_internal import KHITelnetLib
 from queue import Queue
-
-
-@contextmanager
-def time_limit(timeout: int, timeout_handler: Callable[[int, int], Any] = None, disable_for_debug: bool = True):
-    """ Limits execution time to <timeout> seconds.
-        If exceeded, by default raises TimeoutException,
-        or executes timeout_handler(signum, frame) function
-        Usage:
-        with time_limit(timeout):
-            do_something1
-            do_something2
-    """
-    disable_for_debug &= hasattr(sys, 'gettrace') and sys.gettrace() is not None  # Check for debug mode active
-    if timeout_handler is None:
-        def timeout_handler(_, __):
-            raise TimeoutError(f"Execution timed out after {timeout} seconds")
-    signal.signal(signal.SIGBREAK, timeout_handler)
-    timer = threading.Timer(timeout, lambda: signal.raise_signal(signal.SIGBREAK) if not disable_for_debug else None)
-    timer.start()
-    try:
-        yield
-    finally:
-        timer.cancel()
 
 
 class Robot(IRobot, threading.Thread):
