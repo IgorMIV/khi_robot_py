c67587efd0f97ae27850d3fd86828489ecd6e678
aorehov <alexey.orehoff@gmail.com>
2023-12-11 15:08:42 +0300

    minor changes

--- src/robot_exception.py
+++ src/robot_exception.py
@@ -12,7 +12,7 @@ class KawaSyntaxError:
 
     def __init__(self, descr: bytes):
         error_line, error_descr = descr.decode().split("\r\n")
-        self.line = int((error_line_split := error_line.split())[0]) + 1
+        self.line = int((error_line_split := error_line.split())[0])
         self.text = " ".join(error_line_split[1:])
         self.code = error_descr[(code_pos := error_descr.find("^") + 2): code_pos + 5]
         self.descr = error_descr[code_pos + 6:]
@@ -29,7 +29,7 @@ class RobotProgSyntaxError(Exception):
         self.errors = [KawaSyntaxError(error[1:-3]) for error in errors_string if error]
         self.num_errors = len(self.errors)
         error_text = "\n".join([str(error) for error in self.errors])
-        super().__init__(f"File transmission not complete - {self.num_errors} errors found:" + error_text)
+        super().__init__(f"File transmission not complete - {self.num_errors} errors found:\n" + error_text)
 
 
 class RobotProgTransmissionError(Exception):
