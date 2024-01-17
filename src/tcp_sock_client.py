da9a4031bf59faeb16872b68e7ca2cc4484b092c
aorehov <alexey.orehoff@gmail.com>
2024-01-17 13:09:24 +0300

    Renamed telnet_client.py to tcp_sock_client.py because it will be used for tcp/ip connection instead of telnet only

--- src/tcp_sock_client.py
+++ src/tcp_sock_client.py
@@ -1,8 +1,7 @@
 """
-A module for a TelnetClient class that facilitates low-level interaction with a robot over Telnet protocol.
+A module for a TCPSocketClient class that facilitates low-level interaction with a robot over tcp/ip protocol.
 
 Constants:
-    MAX_BYTES_TO_READ (int): Maximum bytes to read in each receive operation.
     RECV_TIMEOUT (int): Receive timeout value in seconds.
     SERVER_TIMEOUT (int): Time limit for connecting to robot
 """
@@ -13,17 +12,16 @@ RECV_TIMEOUT = 1
 SERVER_TIMEOUT = 10
 
 
-class TelnetClient:
+class TCPSocketClient:
     def __init__(self, ip: str, port: int):
-        self._robot_ip = self._validate_ip(ip)                            # The validated IP address of the robot.
-        self._robot_port = self._validate_port(port)                      # The validated port number of the robot.
+        self._robot_ip = ip                                               # IP address of the robot.
+        self._robot_port = port                                           # port number of the robot.
 
         self._client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Socket object for the Telnet connection.
         self._client.settimeout(SERVER_TIMEOUT)                           # Set time limit for connecting to robot
         self._client.connect((self._robot_ip, self._robot_port))
 
     def send_msg(self, cmd: str, end: bytes = b'\n') -> None:
-        """ sends a command to the robot """
         self._client.sendall(cmd.encode() + end)
 
     def send_bytes(self, cmd: bytes) -> None:
@@ -43,18 +41,3 @@ class TelnetClient:
 
     def disconnect(self) -> None:
         self._client.close()
-
-    @staticmethod
-    def _validate_ip(ip) -> str:
-        octets = ip.split(".")
-        if len(octets) != 4:
-            raise ValueError("ip-address must consist of 4 octets divided by dots")
-        if not all(octet.isnumeric() and 0 <= int(octet) <= 255 for octet in octets):
-            raise ValueError("ip-address must consist only of numeric characters with octets being in range [0, 255]")
-        return ip
-
-    @staticmethod
-    def _validate_port(port) -> int:
-        if 0 < port < 65535:
-            return port
-        raise ValueError(f"{port} is not a valid port number")
